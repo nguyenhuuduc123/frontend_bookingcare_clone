8364c458dbb45e22abcbb5c656903e05bd431aaa
duc <duccccnguyen@gmail.com>
2022-09-16 21:00:48 +0700

    49 customize slick react

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -1,63 +1,92 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './Specialty.scss'
-import {FormattedMessage} from 'react-intl';
-import Slider from 'react-slick';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Specialty.scss";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-class Specialty extends Component {
-    
-    render() {  
-        let settings = {
-            dots: false,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-          };
-        return (
-            <div className = "section-specialty">
-                <div className = "specialty-content">
-                <Slider {...settings}>
-      <div className = "img-customize">
-        <h3>1</h3>
-      </div>
-      <div  className = "img-customize">
-        <h3>2</h3>
-      </div>
-      <div  className = "img-customize">
-        <h3>3</h3>
-      </div>
-      <div  className = "img-customize">
-        <h3>4</h3>
-      </div>
-      <div  className = "img-customize">
-        <h3>5</h3>
-      </div>
-      <div  className = "img-customize">
-        <h3>6</h3>
-      </div>
-    </Slider>
-                </div>
-            </div>
-        );
-    }
-
+import specialtyIMG from "../../../assets/images/specialty/picture2.jpg";
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        language : state.app.language,
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
 
+class Specialty extends Component {
+  render() {
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
     };
+    return (
+      <div className="section-specialty">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span>chuyên khoa phổ biến</span>
+            <button> xem thêm</button>
+          </div>
+          <div className="specialty-body">
+            <Slider {...settings}>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+              <div className="specialty-customize">
+                <img src={specialtyIMG} />
+                <div> cơ xương khớp</div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
 };
 
-const mapDispatchToProps = dispatch => {    
-    return {
-        
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
